8719d4b0a9eae20a310d90917a3bfbb9167d7185
Konrad Bonder <Konrad.Bonder@wolterskluwer.com>
2024-08-28 17:23:53 +0200

    feat: update nav link

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const reservationLink = 'https://wynajmij.comfortstorage.eu/';
+export const reservationLink = 'https://wynajmij.comfortstorage.eu/location/1';
 export const rentLink = 'https://wynajmij.comfortstorage.eu/login';
